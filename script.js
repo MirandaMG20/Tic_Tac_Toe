9c49b8d97d574c7596f59a4c242c5ed045f4cd80
MirandaMG20 <guadamiran@gmail.com>
2023-06-28 18:48:02 -0400

    lost

--- script.js
+++ script.js
@@ -5,24 +5,32 @@ const round = document.querySelector(".round"); // selects the class round and a
 const players = document.querySelectorAll(".players>div"); // selects all the div children of the class players
 
 
-
-class TicTacToe { 
+class TicTacToe {
     constructor() {
         this.rounds = (localStorage.getItem("rounds")) ? localStorage.getItem("rounds") : 1;
         this.turn = (localStorage.getItem("turn")) ? localStorage.getItem("turn") : 0;
-        this.currentPlayer = players[this.turn];
+        TicTacToe.changePlayer(players[this.turn]);
         this.boxClasses = (localStorage.getItem("boxClasses")) ? localStorage.getItem("boxClasses").split(",") : this.getBoxClasses();
-        this.setBoxClasses(); 
+        this.setBoxClasses();
     }
 
     static saveGame() { // method that saves the game state in the local browser storage
+        this.boxClasses = this.getBoxClasses();
         localStorage.setItem("rounds", this.rounds);
         localStorage.setItem("turn", this.turn);
         localStorage.setItem("boxClasses", this.boxClasses.join(","));
     }
 
     static resetGame() { //
-        this.boxClasses
+        this.setBoxClasses(true);
+        this.turn = 0;
+        this.changePlayer(players[this.turn]);
+        this.saveGame();
+    }
+
+    static newGame() { //
+        this.rounds = 1;
+        this.resetGame();
     }
 
     static getBoxClasses() { // gets the class names of the boxes and returns as an array
@@ -34,18 +42,27 @@ class TicTacToe {
     }
 
     static setBoxClasses(reset) {
-        TicTacToe.boxClasses.forEach((className, i) => {
+        this.boxClasses.forEach((className, i) => {
             boxes[i].className = (reset) ? "" : className;
         });
     }
 
+    static changePlayer(player) { // switch player
+        debugger;
+        this.currentPlayer.className = "";
+        if (player) {
+            this.currentPlayer = player; // 
+        } else {
+            this.turn = (this.currentPlayer != players[0]) ? 0 : 1;
+            this.currentPlayer = players[this.turn];
+        }
+
+        this.currentPlayer.className = "active";
+        this.saveGame();
+    }
 }
 
-const changePlayer = () => { // switch player
-    TicTacToe.currentPlayer.className = "";
-    TicTacToe.currentPlayer = (TicTacToe.currentPlayer != players[0]) ? players[0] : players[1]; // current is not equal  player [0] ? 
-    TicTacToe.currentPlayer.className = "active";
-}
+const game = new TicTacToe();
 
 const showAlert = (message) => {
     const div = document.createElement('div'); // creates a div in the void/document  
@@ -58,7 +75,7 @@ const showAlert = (message) => {
 
 const evalGame = () => { //
 
-    const match = TicTacToe.currentPlayer.id + TicTacToe.currentPlayer.id + TicTacToe.currentPlayer.id;
+    const match = game.currentPlayer.id + game.currentPlayer.id + game.currentPlayer.id;
 
     if (
         boxes[0].className + boxes[1].className + boxes[2].className == match ||  // 
@@ -70,31 +87,22 @@ const evalGame = () => { //
         boxes[0].className + boxes[4].className + boxes[8].className == match ||
         boxes[2].className + boxes[4].className + boxes[6].className == match
     ) {
-        showAlert(TicTacToe.currentPlayer.innerHTML + " Winner!")
+        showAlert(game.currentPlayer.innerHTML + " Winner!")
     } else {
-        changePlayer();
+        game.changePlayer();
     }
 }
-
 
 const selectBox = (clickEvent) => {
     const selectedBox = clickEvent.target; // equals the current box selected
     if (selectedBox.className) {  // if selected box already has a class, return 
         return;
     }
-    selectedBox.className = TicTacToe.currentPlayer.id; // setting the class name of the box to the current player id
+    selectedBox.className = game.currentPlayer.id; // setting the class name of the box to the current player id
     evalGame(); // see if the current player won 
-}
-
-const deselectBox = () => {
-
-}
-
-const deselectAllBox = () => {
-
+    game.saveGame();
 }
 
 boxes.forEach((box) => {
     box.addEventListener("click", selectBox);
 });
-
